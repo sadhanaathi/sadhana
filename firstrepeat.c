91677f45da628f3bdeeba2fd958f023f53727b6e
sadhanaathi <sadhanaathi77@gmail.com>
2016-06-12 12:59:25 +0530

    Create firstrepeat.c

---
+++ firstrepeat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,j;
+    for(i=0;i<n;i++)
+      scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                printf("%d",a[i]);
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
